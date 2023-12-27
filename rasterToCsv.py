7e54ee75d5dc46cdcf1fc8b54d07a2c4473f31ff
hahaIrene <ip7112533@gmail.com>
2023-12-27 21:54:46 +0800

    renew raster to csv

--- rasterToCsv.py
+++ rasterToCsv.py
@@ -1,29 +1,46 @@
 from PIL import Image
 import numpy as np
-import os
 
-# 定義資料夾路徑
-folder_path = './data'
+# 定義檔案路徑和檔案名稱
+file_path = r'./data'
+file_names = ['red.tif', 'blue.tif', 'green.tif', 'nir.tif',
+              'Var_red.tif', 'Var_blue.tif', 'Var_green.tif', 'Var_nir.tif',
+              'Cont_red.tif', 'Cont_blue.tif', 'Cont_green.tif', 'Cont_nir.tif',
+              'Corelation_red', 'Corelation_blue', 'Corelation_green', 'Corelation_nir',
+              'Homo_red.tif', 'Homo_blue.tif', 'Homo_green.tif', 'Homo_nir.tif',
+              'Var_red_90.tif', 'Var_blue_90.tif', 'Var_green_90.tif', 'Var_nir_90.tif',
+              'Cont_red_90.tif', 'Cont_blue_90.tif', 'Cont_green_90.tif', 'Cont_nir_90.tif',
+              'Corelation_red_90', 'Corelation_blue_90', 'Corelation_green_90', 'Corelation_nir_90',
+              'Homo_red_90.tif', 'Homo_blue_90.tif', 'Homo_green_90.tif', 'Homo_nir_90.tif',
+              ]
 
-# 獲取資料夾中所有的檔案
-file_list = os.listdir(folder_path)
+# 讀取 TIFF 檔案並轉換為 NumPy 陣列
+arrays = [np.array(Image.open(file_path + file)) for file in file_names]
 
-# 儲存所有圖片資料的列表
-image_arrays = []
-
-# 依次讀取並處理圖片檔案
-for file_name in file_list:
-    if file_name.endswith('.tiff'):
-        # 讀取圖片並轉換成 NumPy 陣列
-        img = Image.open(os.path.join(folder_path, file_name))
-        img_array = np.array(img)
-        
-        # 將圖片陣列加入列表
-        image_arrays.append(img_array.ravel())
-
-# 將所有圖片陣列堆疊成一個二維陣列
-stacked_array = np.column_stack(image_arrays)
+# 將陣列重新排列並堆疊
+stacked_array = np.column_stack(tuple(arr.ravel() for arr in arrays))
 
 # 儲存成 CSV 檔案
-np.savetxt(folder_path + 'output.csv', stacked_array.T, delimiter=',', fmt='%d')
+np.savetxt(file_path + 'output.csv', stacked_array, delimiter=',', fmt='%d')
+
+
+
+# # 定義固定的路徑
+# file_path = '/your/directory/path/'
+
+# # 讀取 TIFF 檔案
+# tiff1 = Image.open(file_path + 'tiff1.tiff')
+# tiff2 = Image.open(file_path + 'tiff2.tiff')
+# tiff3 = Image.open(file_path + 'tiff3.tiff')
+
+# # 將 TIFF 轉換為 NumPy 陣列
+# array1 = np.array(tiff1)
+# array2 = np.array(tiff2)
+# array3 = np.array(tiff3)
+
+# # 將陣列重新排列並堆疊
+# stacked_array = np.column_stack((array1.ravel(), array2.ravel(), array3.ravel()))
+
+# # 儲存成 CSV 檔案
+# np.savetxt(file_path + 'output.csv', stacked_array, delimiter=',', fmt='%d')
 
